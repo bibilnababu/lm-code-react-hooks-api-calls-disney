44521ab064ebcd50c03d507c2a92ce9692fd4f49
Bibilna Babu <bibilnababu@yahoo.com>
2023-03-20 18:00:17 +0000

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import Header from "./components/header";
 import CharacterContainer from "./components/character_container";
 import Navigation from "./components/navigation";
 import { DisneyCharacter } from "./disney_character";
-
+import React, { useState, useEffect } from 'react';
 
 const App: React.FC = () => {
   
@@ -12,23 +12,18 @@ const App: React.FC = () => {
 
     // Some dummy state representing disney characters
     const [characters, setCharacters] = useState<Array<DisneyCharacter>>([
-      {
-        _id: 6,
-        name: "'Olu Mel",
-        imageUrl: "https://static.wikia.nocookie.net/disney/images/6/61/Olu_main.png"
-      },
-      {
-        _id: 25,
-        name: "Abu",
-        imageUrl: "https://static.wikia.nocookie.net/disney/images/3/3f/Profile_-_Abu.png"
-      },
-      {
-        _id: 30,
-        name: "Ace",
-        imageUrl: "https://static.wikia.nocookie.net/disney/images/1/1e/Profile_-_Ace.png"
-      },
+      
     ]);
-
+    
+    useEffect(() => {
+      const getCharacters = async (pageNumber : number) => {
+        const apiResponse = await fetch(`http://api.disneyapi.dev/characters?page=${pageNumber}`);
+        const json = await apiResponse.json() as { data: DisneyCharacter[] };
+          setCharacters(json.data);
+      };
+        getCharacters(1);
+      }, []);
+ 
   return (
         <div className="page">
           <Header currentPage={currentPage} />
